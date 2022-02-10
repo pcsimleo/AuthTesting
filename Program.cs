7c2c39a399356a274d9485d56251994d3fd74c3a
pcsimleo <pcsimleo@upc.edu.pe>
2022-02-10 17:12:00 -0500

    AuthTesting

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AuthTesting
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static string Something() 
+        {
+            return "Algo";
+        }
+
+
+        public static bool login(string user, string pass) =>
+            user == "Mleon" && pass == "123456" ? true : false;
+    }
+}
